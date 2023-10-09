947bc8c15b28895441e91895057d64de040f1d7b
Vilius Ivanovas <ivanovasvilius@gmail.com>
2023-10-09 14:49:18 +0200

    Trying to set up a system to post comments

--- app/backend/auth/comments.php
+++ app/backend/auth/comments.php
@@ -11,7 +11,39 @@ if (!Input::get('post_id')) {
 $post_id = Input::get('post_id');
 $post = Post::getPostById($post_id);
 
-
 $comments = Comment::getAllComments($post_id);
 // This file checks if the user is logged in and if the post_id is set. If not, it redirects to the index.php page. 
 //If the user is logged in and the post_id is set, it gets the post and all the comments for that post. The comments are then displayed in the comments.php file.
+
+if (Input::exists()) {
+    if (Token::check(Input::get('csrf_token'))) {
+        $validate = new Validation();
+
+        $validation = $validate->check($_POST, array(
+            'content' => array(
+                'required' => true,
+                'min' => 1,
+                'max' => 1024,
+            ),
+        ));
+
+        if ($validate->passed()) {
+            try {
+                $comments->create(array(
+                    'user_id'  => $user->data()->uid,
+                    'post_id'  => $post_id,
+                    'content'  => Input::get('content'),
+                    'created_at'=> date('Y-m-d H:i:s'), //AMERICAN DATE FORMAT CHANGE TO EUROPEAN ONE DAY!
+                ));
+                Session::flash('register-success', 'Thanks for registering! You can login now.');
+                Redirect::to('index.php');
+            } catch (Exception $e) {
+                die($e->getMessage());
+            }
+        } else {
+            foreach ($validate->errors() as $error) {
+                echo '<div class="alert alert-danger"><strong></strong>' . cleaner($error) . '</div>';
+            }
+        }
+    }
+}
--- app/frontend/pages/comments.php
+++ app/frontend/pages/comments.php
@@ -3,20 +3,28 @@
         <div class="jumbotron text-center" style="margin-bottom:0">
             <h1><?php echo $post->title; ?></h1>
 
-                <p> <?php echo $post->content; ?> </p>
+            <p> <?php echo $post->content; ?> </p>
 
-                <h3>The Comment Section</h3>
+            <h3>The Comment Section</h3>
 
-                <?php
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="comment"></label>
+                    <input type="text" class="form-control" id="comment" placeholder="Comment" name="comment">
+                </div>
+                <input type="submit" class="btn-register" value="Post comment">
+                <input type="hidden" name="csrf_token" value="<?php echo Token::generate(); ?>">
+            </form>
 
-                if ($comments->count()) {
-                    foreach ($comments->results() as $c) {
-                        echo '<div class="alert alert-info"><strong>' . (new User($c->user_id))->data()->username . '</strong>: ' . $c->content . '</div>';
-                    }
-                } else {
-                    echo '<div class="alert alert-danger"><strong></strong>No comments found!</div>';
+            <?php
+            if ($comments->count()) {
+                foreach ($comments->results() as $c) {
+                    echo '<div class="alert alert-info"><strong>' . (new User($c->user_id))->data()->username . '</strong>: ' . $c->content . '</div>';
                 }
-                ?>
+            } else {
+                echo '<div class="alert alert-danger"><strong></strong>No comments found!</div>';
+            }
+            ?>
         </div>
     </div>
 </div>
