af8d7e496df7204d6340fe8ff7c3179fef15e465
Vilius Ivanovas <ivanovasvilius@gmail.com>
2023-10-30 12:46:12 +0100

    Edited text on create post site to say edit post when editing a post

--- app/frontend/pages/create-post.php
+++ app/frontend/pages/create-post.php
@@ -1,5 +1,12 @@
 <div class="container" style="padding-top: 5%; padding-bottom: 5%;">
-    <h2>Create Post Form</h2>
+    <?php
+    if (isset($_GET['post_id'])) {
+        $heading = 'Edit Post "' . $_GET['post_id'] . '"';
+    } else {
+        $heading = 'Create Post';
+    }
+    ?>
+    <h2><?php echo $heading; ?></h2>
     <form action="" method="post">
         <div class="form-group">
             <label for="name">Title :</label>
@@ -13,7 +20,7 @@
             <label for="description">Images :</label>
             <input type="text" class="form-control" id="image" placeholder="Insert an image source link (optional)" name="image" value="<?php echo escape(Input::get('image')); ?>">
         </div>
-        <input type="submit" class="btn-register" value="Create a post">
+        <input type="submit" class="btn-register" value="<?php echo $heading; ?>">
         <input type="hidden" name="csrf_token" value="<?php echo Token::generate(); ?>">
     </form>
 </div>
